051b09af4da1a8171337ae9b7cb0afaf1cda2bd0
switchcrit <62515128+switchcrit@users.noreply.github.com>
2022-12-12 18:35:15 +0530

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/Ejp0gjbZ2hj3ynFp/scene.splinecode');
